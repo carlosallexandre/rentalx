f34b8439f7652333a02aef1f4885386713035659
carlosallexandre <carlosallexandre@pop-os.localdomain>
2022-02-16 15:40:56 -0300

    docs: add swagger-ui

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { routes } from "./routes";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3333, () => console.log("Server is running!"));
