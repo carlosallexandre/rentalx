6346bed821444f3996dac52e0398e25291438354
carlosallexandre <carlosallexandre@pop-os.localdomain>
2022-04-02 17:36:52 -0300

    chore: config postgres database

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
+import "dotenv/config";
+import "reflect-metadata";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
+import db from "./database";
 import { routes } from "./routes";
 import swaggerDocument from "./swagger.json";
 
 const app = express();
+
+db.initialize();
 
 app.use(express.json());
 app.use(routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3333, () => console.log("Server is running!"));
+
+process.on("exit", async () => {
+  await db.destroy();
+});
